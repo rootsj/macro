6b6e35a38a5562fe0778d60e6e37cf6b82533c6a
이종화 <rootsj@hiconsy.com>
2022-06-13 16:16:11 +0900

    m1 :

--- car_macro.py
+++ car_macro.py
@@ -1,5 +1,3 @@
-from ctypes import pointer
-from tkinter.messagebox import NO
 from unittest.result import failfast
 
 import pyautogui as pag
@@ -10,16 +8,16 @@ import pytesseract
 import cv2
 import os
 
+starttime = time.time()
 
 while True:
-    starttime = time.time()
     print('while- start')
 
     refresh = pag.locateCenterOnScreen('static/stop.jpg', confidence=0.9, grayscale = True)
     print("refresh object")
     print(refresh)
     print('---------')
-    start = pag.locateCenterOnScreen('static/start.jpg', confidence =0.9)
+    start = pag.locateCenterOnScreen('static/start.png', confidence =0.9)
     print("start object")
     print(start)
     print('---------')
@@ -33,10 +31,10 @@ while True:
 
 
     if start is not None:
-        opt1 = pag.locateCenterOnScreen('static/op1.png')
+        opt1 = pag.locateCenterOnScreen('static/op1.png', confidence=0.98)
         print(opt1)
         pag.click(opt1)
-        opt2 = pag.locateCenterOnScreen('static/op2.png')
+        opt2 = pag.locateCenterOnScreen('static/op2.png', confidence=0.98)
         print(opt2)
         pag.click(opt2)
         
@@ -45,37 +43,41 @@ while True:
         # pag.click(opt3)
 
 
-        slct = pag.locateCenterOnScreen('static/selectoption.jpg', confidence=0.9, grayscale = True)
+        slct = pag.locateCenterOnScreen('static/selectoption.png', confidence=0.9, grayscale = True)
         print("slct")
         print(slct)
 
-        pag.click(slct[0], slct[1]-10)
+        pag.click(slct[0], slct[1]-20)
 
-        slct1 = pag.locateCenterOnScreen('static/doichi_motors.png', confidence=0.9, grayscale = True)
+        slct1 = pag.locateCenterOnScreen('static/opop1.png', confidence=0.97, grayscale = True)
+        print(slct1)
         if slct1 is not None:
             pag.click(slct1)
         else:
-            pag.click(slct[0], slct[1]+25)
+            pag.click(slct[0], slct[1]+5)
         
         pag.click(slct[0], slct[1]+10)
-        slct2 = pag.locateCenterOnScreen('static/doichi_motors_dachi.png', confidence=0.9, grayscale = True)
+        slct2 = pag.locateCenterOnScreen('static/opop2.png', confidence=0.97, grayscale = True)
 
+        print(slct2)
         if slct1 is not None:
             pag.click(slct2)
         else:
-            pag.click(slct[0], slct[1]+60)
+            pag.click(slct[0], slct[1]+50)
 
         pag.click(start)
         break
 
+
 while True:
-    checkImg = pag.locateOnScreen('static/captureok.jpg', confidence =0.9, grayscale = True)
+    checkImg = pag.locateOnScreen('static/captureok.png', confidence =0.9, grayscale = True)
     
+    print(checkImg)
 
     if checkImg is not None:
         print('chapture')
 
-        path = "C:/test/capture/" + str(starttime) + ".png"
+        path = "./capture/" + str(starttime) + ".png"
         qs = pag.locateOnScreen('static/qs.jpg', confidence =0.9, grayscale = True)
         x = qs[0] - 200
         y = qs[1] - 3
@@ -90,9 +92,7 @@ while True:
         pag.click((pag.center(checkImg)[0]-80, pag.center(checkImg)[1]))
         pag.typewrite(text[:-1])
         
-        time.sleep(0.1)
-
-        caps = pag.locateCenterOnScreen('static/captureok.jpg', confidence =0.9, grayscale = True)
+        caps = pag.locateCenterOnScreen('static/captureok.png', confidence =0.9, grayscale = True)
         print(text)
         print(caps)
         pag.click(caps)
--- car_macro_option.py
+++ car_macro_option.py
@@ -3,14 +3,31 @@ import time
 
 
 
+# while True:
+#     opt1 = pag.locateCenterOnScreen('static/op1.png', confidence=0.98)
+#     print(opt1)
+#     pag.moveTo(opt1)
+#     pag.click(opt1)
+#     time.sleep(0.5)
+#     opt2 = pag.locateCenterOnScreen('static/op2.png', confidence=0.98)
+#     print(opt2)
+#     pag.moveTo(opt2)
+#     pag.click(opt2)
+#     time.sleep(0.5)
+
+
 while True:
-    opt1 = pag.locateCenterOnScreen('static/op1.png', confidence=0.95)
-    print(opt1)
-    pag.moveTo(opt1)
-    pag.click(opt1)
+    slct = pag.locateCenterOnScreen('static/selectoption.png', confidence=0.9)
+
+    print('dddstart')
+    pag.moveTo(slct[0], slct[1]-20)
     time.sleep(0.5)
-    opt2 = pag.locateCenterOnScreen('static/op2.png', confidence=0.95)
-    print(opt2)
-    pag.moveTo(opt2)
-    pag.click(opt2)
+
+    pag.moveTo(slct[0], slct[1]+5)
+    time.sleep(0.5)
+    
+    pag.moveTo(slct[0], slct[1]+10)
+    time.sleep(0.5)
+    
+    pag.moveTo(slct[0], slct[1]+50)
     time.sleep(0.5)
